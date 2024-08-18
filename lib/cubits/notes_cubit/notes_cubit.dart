e9f52231aaca12ef4495c2ae80baffe7e303093e
belal_soliman <belalsoliman8@gmail.com>
2024-08-19 01:57:09 +0300

    Add Hive integration for fetching notes in NotesCubit and update HomePage to use BlocProvider
    - Integrated Hive for local storage in NotesCubit to fetch notes asynchronously.
    - Updated NotesCubit to emit NotesSuccess state with fetched notes or NotesError state in case of failure.
    - Modified HomePage to use BlocProvider for NotesCubit, ensuring the cubit is available throughout the widget tree.
    - Updated HomePage to include a floating action button and an app bar with a search icon, enhancing the UI.

--- lib/cubits/notes_cubit/notes_cubit.dart
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 import 'package:notesy/models/note_model.dart';
 
@@ -6,4 +7,13 @@ part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+  fetchNote() async {
+    try {
+      var notesBox = Hive.box<NoteModel>('notes');
+
+      emit(NotesSuccess(notes: notesBox.values.toList()));
+    } catch (e) {
+      emit(NotesError(error: e.toString()));
+    }
+  }
 }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesy/cubits/notes_cubit/notes_cubit.dart';
 import 'package:notesy/views/notes_view.dart';
 
 import 'package:notesy/views/widget/floating_btn.dart';
@@ -8,42 +10,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: const FloatingBtn(),
-      appBar: AppBar(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(20),
-          ),
-        ),
-        elevation: 0,
-        title: const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Notes',
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey),
-            )),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.search,
-              size: 40,
-              color: Color.fromARGB(255, 23, 93, 128),
+    return BlocProvider(
+      create: (context) => NotesCubit(),
+      child: Scaffold(
+        floatingActionButton: const FloatingBtn(),
+        appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(20),
             ),
-          )
-        ],
-      ),
-      body: const Column(
-        children: [
-          SizedBox(
-            height: 5,
           ),
-          NotesView(),
-        ],
+          elevation: 0,
+          title: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Notes',
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey),
+              )),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                size: 40,
+                color: Color.fromARGB(255, 23, 93, 128),
+              ),
+            )
+          ],
+        ),
+        body: const Column(
+          children: [
+            SizedBox(
+              height: 5,
+            ),
+            NotesView(),
+          ],
+        ),
       ),
     );
   }

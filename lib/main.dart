b20a3037925af30cea33c0a101b20519c9cac7e0
belal_soliman <belalsoliman8@gmail.com>
2024-08-19 00:16:08 +0300

    Add SimpleBlocObserver for Enhanced Bloc Monitoring
    This commit introduces a `SimpleBlocObserver` class that implements the `BlocObserver` interface to monitor and log various lifecycle events and changes within blocs. The observer currently logs changes to the console for debugging purposes and leaves other methods unimplemented for future extension.
    
    Additionally, the main application initialization now sets this observer as the global observer for all blocs, enhancing the ability to track and debug bloc-related activities across the application.

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notesy/bloc_observor.dart';
 import 'package:notesy/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notesy/models/note_model.dart';
 import 'package:notesy/views/home_page.dart';
 
 void main() async {
+  Bloc.observer = SimpleBlocObserver();
   await Hive.initFlutter();
   await Hive.openBox('notes');
   Hive.registerAdapter(NoteModelAdapter());

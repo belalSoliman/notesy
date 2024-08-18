f4c4c0787163b6fe1727cd65a7cd23d2f1776816
belal_soliman <belalsoliman8@gmail.com>
2024-08-18 15:53:42 +0300

    Add Custom TextField to ModalSheet Widget
    This commit introduces a new CustomTextField widget and integrates it into the
    ModalSheet widget. The CustomTextField widget is designed to be flexible,
    allowing for a variable number of lines and a customizable title. It features
    styling for filled background, border, and focused states to enhance user
    experience. The ModalSheet widget now includes two instances of the
    CustomTextField: one for a single-line title input and another for a
    multi-line note input. This enhancement improves the functionality and
    usability of the ModalSheet, providing a clear and structured input interface
    for users.

--- lib/views/widget/modal_sheet.dart
+++ lib/views/widget/modal_sheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'text_field.dart';
+
 class ModalSheet extends StatelessWidget {
   const ModalSheet({super.key});
 
@@ -7,9 +9,19 @@ class ModalSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         height: MediaQuery.of(context).size.height * 0.7,
-        width: MediaQuery.of(context).size.width * 0.9,
-        child: const Column(
-          children: [],
+        width: MediaQuery.of(context).size.width * 1,
+        child: Column(
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.05),
+            const CustoemTextFiled(
+              title: 'Title',
+              lines: 1,
+            ),
+            const CustoemTextFiled(
+              title: 'your note',
+              lines: 5,
+            ),
+          ],
         ));
   }
 }
---
+++ lib/views/widget/text_field.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustoemTextFiled extends StatelessWidget {
+  const CustoemTextFiled({super.key, required this.title, required this.lines});
+  final String title;
+  final int lines;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+          maxLines: lines,
+          style: const TextStyle(color: Colors.black),
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: const BorderSide(color: Colors.white),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: const BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: const BorderSide(color: Colors.greenAccent),
+            ),
+            hintText: title,
+            hintStyle: const TextStyle(color: Colors.grey),
+          )),
+    );
+  }
+}
